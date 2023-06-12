ff5880caa7cdcb49cee5fde642649079e5217abb
Roshan <roshansalian1997@gmail.com>
2023-06-12 22:27:16 +0530

    Roshan

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,36 @@
 */
 
 function isAnagram(str1, str2) {
+  let dict = {};
 
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+
+  for(var cha of str1){
+    if(dict[cha]){
+      dict[cha]++;
+    }
+    else {
+      dict[cha]=1;
+    }
+  }
+
+  for(var cha of str2){
+    if(dict[cha]==null || dict[cha]<=0){
+      return false;
+    }
+    else{
+      dict[cha]--;
+
+      if(dict[cha]==0){
+        delete dict[cha];
+      }
+    }
+  }
+  var sizw = Object.keys(dict).length;
+  if(sizw == 0)
+    return true;
+  else return false;
 }
 
 module.exports = isAnagram;
